3ddabed2357c0494009c24c63dfe0d29452b5d5e
ShineShao <xiaoshaoqq@gmail.com>
2025-11-14 22:21:29 +0800

    demo: update demo and update ezuikit-flv@2.0.3

--- examples/vue3.3-app/src/components/Player.vue
+++ examples/vue3.3-app/src/components/Player.vue
@@ -9,8 +9,8 @@ onMounted(() => {
   player = new EzuikitFlv({
     container: document.getElementById('container'),
     debug: true,
-    url: 'https://rtmp05open.ys7.com:9188/v3/openlive/GG6358043_1_1.flv?expire=1763125058&id=909532552559247360&t=6dfd150f733f58117b29f99817ffb3a7aec648d1cf6fa611ca8c5fb87041656e&ev=101&supportH265=1',
-    useMSE: false,
+    url: 'https://rtmp05open.ys7.com:9188/v3/openlive/GG6358043_1_1.flv?expire=1763149791&id=909560789419950080&t=6ac3ea24ddb5997c6c91509d584f040d9e5184c44846ef814a88fdd9aac7667d&ev=101&supportH265=1',
+    // useMSE: false,
     autoPlay: true,
     // decoder: 'decoder.js' // 软解解码资源 （wasm 要和js 在同一个文件夹中）
     // themeData: null // 忽略主题 @since 2.0.0
